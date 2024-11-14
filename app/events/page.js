23a312b134a351e1338db28e55fc7f8927f4d4bd
Joy-mwende63 <mwendejoy47@gmail.com>
2024-11-14 14:21:42 +0300

    events

--- app/events/page.js
+++ app/events/page.js
@@ -4,7 +4,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './Events.module.css';
-import Navbar from
+import Navbar from 
 
 const EventsPage = () => {
   const [events, setEvents] = useState([]);
