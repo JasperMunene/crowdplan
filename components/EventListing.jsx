24cb83f83b7a9b27f131150b53d8b0eb61749d9e
Jasper Munene <132051381+JasperMunene@users.noreply.github.com>
2024-11-15 13:41:06 +0300

    Update EventListing.jsx

--- components/EventListing.jsx
+++ components/EventListing.jsx
@@ -11,7 +11,7 @@ export default function EventListing() {
   const [events, setEvents] = useState(null);
 
   useEffect(() => {
-    fetch("https://event-api-9ll8.onrender.com/events")
+    fetch("http://localhost:3000/events")
       .then((response) => response.json())
       .then((data) => {
         setEvents(data);
